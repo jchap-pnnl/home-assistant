826575c5f56ab9eb994ae7a8129fc2e3857b207b
Chapman <joseph.chapman@pnnl.gov>
2017-08-28 17:13:55 -0700

    started the advanced settings card and component

---
+++ config/custom_components/advanced_settings/__init__.py
@@ -0,0 +1,96 @@
+"""
+The "advanced settings" component.
+
+"""
+
+import asyncio
+import logging
+import os
+import json
+from datetime import timedelta
+from homeassistant.config import load_yaml_config_file
+from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity_component import EntityComponent
+from homeassistant.core import State
+
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "advanced_settings"
+FROM = "from"
+SCAN_INTERVAL = timedelta(3600)
+
+DEPENDENCIES = []
+
+def setup(hass, config):
+    """Setup our skeleton component."""
+
+    _LOGGER.info("Advanced Settings loading.")
+    
+    component = EntityComponent(_LOGGER, DOMAIN, hass, SCAN_INTERVAL)
+
+    component.setup(config)
+
+    descriptions = load_yaml_config_file(
+        os.path.join(os.path.dirname(__file__), 'services.yaml'))
+
+    def update_advanced_settings(service):
+        """Do any update to the component."""
+        
+        update_obj = service.data.get('value')
+
+        _LOGGER.info("update object: %s", update_obj)
+
+        advanced_settings = hass.states.get('advanced_settings.advanced_settings').as_dict()
+        attributes = advanced_settings["attributes"]
+
+        if update_obj["subtarget"] is not None:
+            attributes[update_obj["target"]][update_obj["subtarget"]] = update_obj["value"]
+        else:
+            attributes[update_obj["target"]] = update_obj["value"]
+
+        hass.states.set('advanced_settings.advanced_settings', State.from_dict(advanced_settings), attributes, True)
+
+        _LOGGER.info("advanced settings after update: %s", hass.states.get('advanced_settings.advanced_settings'))
+
+    hass.services.register(
+        DOMAIN,
+        'update_advanced_settings',
+        update_advanced_settings,
+        descriptions['update_advanced_settings'])
+
+    return True
+
+
+class AdvancedSettingsComponent(Entity):
+    """Representation of a Sensor."""
+
+    def __init__(self):
+        """Initialize the sensor."""
+
+        _LOGGER.info("AdvancedSettingsPlatform loading.")
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Advanced Settings'
+
+    @property
+    def state(self):
+        """Card state."""
+        return 'happy'
+
+    @property
+    def state_attributes(self):
+        """Return the optional state attributes."""
+
+        data = {
+            "energySavings":{
+                "goal": "250",
+                "unit": "Kw"
+            },
+            "savingsStartTime": "10:00 a.m.",
+            "savingsEndTime": "5:00 p.m."
+        }
+
+        return data
---
+++ config/custom_components/advanced_settings/advanced_settings_platform.py
@@ -0,0 +1,44 @@
+"""
+The "advanced settings platform" platform.
+
+"""
+
+import logging
+from custom_components.advanced_settings import AdvancedSettingsComponent
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Setup the sensor platform."""
+
+    _LOGGER.info("Setting up AdvancedSettingsPlatform.")
+
+    add_devices([AdvancedSettingsPlatform()])
+
+
+class AdvancedSettingsPlatform(AdvancedSettingsComponent):
+    """Representation of a demo climate device."""
+
+    def __init__(self):
+        """Initialize the climate device."""
+        self._state = 'On'
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Advanced Settings'
+
+    def set_advanced_settings(self: AdvancedSettingsComponent, new_value):
+        _LOGGER.info("In component set_advanced_settings method")
+        # _LOGGER.info("slider value: %s", new_value)
+        # self._state = {'slider_value': new_value.slider_value}
+        # _LOGGER.info("state: %s", self._state)
+        # self.state_attributes["device"][0]["flexibility"] = new_value
+        # _LOGGER.info("advanced settings component device flexibility: %s", self.state_attributes["device"][0]["flexibility"])
+
